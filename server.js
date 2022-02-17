75104ffc1845250b28e7560e02b1feda45798bbc
Joshua Reyes <reyes.j15@gmail.com>
2022-02-17 17:45:05 -0500

    added /albums route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Data Layer - to be moved to db.js
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/music_db');
 const STRING = Sequelize.DataTypes.STRING;
@@ -24,6 +25,42 @@ const Album = sequelize.define('album', {
 }); //build Album model
 
 Album.belongsTo(Artist);
+//express
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => res.redirect('/albums'));
+
+app.get('/albums', async(req,res,next) => {
+    try {
+        const albums = await Album.findAll({
+            include: [ Artist ]
+        });
+        const html = albums.map( album => {
+            return `
+                <div>
+                    ${ album.name }
+                    <a href='/artists/${ album.artistId }'>${ album.artist.name } </a>
+                </div>
+            `;
+        }).join('');
+        res.send(`
+        <html>
+            <head>
+                <title> Discography </title>
+            </head>
+            <body>
+            <h1> Discography </h1>
+            ${ html }
+            </body>
+        </html>
+        `);
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
 const start = async()=> {
     try{
         await sequelize. sync( {force: true} );
@@ -33,7 +70,9 @@ const start = async()=> {
         const taylorSwift = await Artist.create({name: 'Taylor Swift'});
         const red = await Album.create({name: 'Red', artistId: taylorSwift.id});
         const dawnFm = await Album.create({name: 'Dawn FM', artistId: theWeeknd.id});
-        const ntws = await Album.create({name: 'Nothing was the Same', artistId: drake.id});
+        const nwts = await Album.create({name: 'Nothing was the Same', artistId: drake.id});
+        const port = process.env.PORT || 3000;
+        app.listen(port, ()=> console.log(`listening on port ${port}`));
         console.log(drake);
     }
     catch(ex) {
